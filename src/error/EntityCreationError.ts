4f4e096a48679083831bfa33a39a8718f182fd70
Jonn Nilsson <jonn@jonn.nu>
2021-05-28 08:20:20 +0200

    fix: add accessibility modifier

--- src/error/EntityCreationError.ts
+++ src/error/EntityCreationError.ts
@@ -1,7 +1,7 @@
 import { ExtendableError } from "@lindorm-io/errors";
 
 export class EntityCreationError extends ExtendableError {
-  constructor(entityName: string) {
+  public constructor(entityName: string) {
     super(`Unable to create Entity ${entityName}`, {
       details: "The create() event has already been called.",
     });
