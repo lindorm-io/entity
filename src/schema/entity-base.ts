3a9441d65437723ea60a5b82a3efe108a691a69d
Jonn Nilsson <jonn@jonn.nu>
2021-05-28 08:18:31 +0200

    feat: add schema export

---
+++ src/schema/entity-base.ts
@@ -0,0 +1,17 @@
+import Joi from "joi";
+
+export const JOI_EVENTS = Joi.array().items(
+  Joi.object({
+    date: Joi.date().required(),
+    name: Joi.string().required(),
+    payload: Joi.object().required(),
+  }),
+);
+
+export const JOI_ENTITY_BASE = {
+  id: Joi.string().guid({ version: "uuidv4", separator: "-" }).required(),
+  created: Joi.date().required(),
+  events: JOI_EVENTS.required(),
+  updated: Joi.date().required(),
+  version: Joi.number().integer().min(0).required(),
+};
