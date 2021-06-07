453e959f3e4b33712877f63ed2f251166c345c03
Jonn Nilsson <jonn@jonn.nu>
2021-06-07 15:48:44 +0200

    fix: update entity creation error

--- src/error/EntityCreationError.ts
+++ src/error/EntityCreationError.ts
@@ -3,7 +3,9 @@ import { ExtendableError } from "@lindorm-io/errors";
 export class EntityCreationError extends ExtendableError {
   public constructor(entityName: string) {
     super(`Unable to create Entity ${entityName}`, {
-      details: "The create() event has already been called.",
+      debug: {
+        notes: "The create() event has already been called.",
+      },
     });
   }
 }
