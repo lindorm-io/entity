51465cf7170523ae169b14654cd2ec6e7d30dddb
Jonn Nilsson <jonn@jonn.nu>
2021-06-12 08:56:18 +0200

    fix: extend error from lindorm error

--- src/error/EntityCreationError.ts
+++ src/error/EntityCreationError.ts
@@ -1,11 +1,9 @@
-import { ExtendableError } from "@lindorm-io/errors";
+import { LindormError } from "@lindorm-io/errors";
 
-export class EntityCreationError extends ExtendableError {
+export class EntityCreationError extends LindormError {
   public constructor(entityName: string) {
     super(`Unable to create Entity ${entityName}`, {
-      debug: {
-        notes: "The create() event has already been called.",
-      },
+      debug: { notes: "The create() event has already been called." },
     });
   }
 }
